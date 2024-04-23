86338f9e16b9bb8c4cb35f6842c6553746e29b34
Ivan Burulev <167755067+9Ivan47@users.noreply.github.com>
2024-04-23 23:38:00 +0700

    First commit

---
+++ TestingLR2/TestingLR2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace TestingLR2
+{
+    public class MinDigit
+    {
+        public static int Minim(int a)
+        {
+            int min = a % 10;
+            a /= 10;
+            while (a != 0)
+            {
+                if (a % 10 < min)
+                {
+                    min = a % 10;
+                }
+                a /= 10;
+            }
+            return min;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите натуральное число: ");
+            int a = int.Parse(Console.ReadLine());
+            while (a <= 0)
+            {
+
+                Console.WriteLine("Ошибка!!!");
+                a = int.Parse(Console.ReadLine());
+
+            }
+            MinDigit min = new MinDigit();
+            double s = MinDigit.Minim(a);
+            Console.WriteLine("Минимальная цифра: " + s);
+        }
+    }
+}
